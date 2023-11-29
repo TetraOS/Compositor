c378884599bb6a78e9f2dd6f959cd180a02b1851
Carson Schildt <128015493+cruxschildt@users.noreply.github.com>
2023-11-28 18:40:41 -0700

    Update compositor.h

--- src/core/compositor/compositor.h
+++ src/core/compositor/compositor.h
@@ -1,1 +1,24 @@
+// This is currently a base file content, and more code will be later
 
+#pragma once
+
+#include <QObject>
+
+class Compositor : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit Compositor(QObject *parent = nullptr);
+
+    // Add any public functions or variables here
+
+signals:
+    // Add any signals here
+
+public slots:
+    // Add any public slots here
+
+private:
+    // Add any private functions or variables here
+};
