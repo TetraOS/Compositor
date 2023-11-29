1b347b39465e79fc1de07c073f932ad4c5617bdc
Carson Schildt <128015493+cruxschildt@users.noreply.github.com>
2023-11-28 18:42:19 -0700

    Update compositor.cpp

--- src/core/compositor/compositor.cpp
+++ src/core/compositor/compositor.cpp
@@ -1,1 +1,13 @@
+// This is just the base code, more code will be added later
 
+#include "compositor.h"
+
+Compositor::Compositor(QObject *parent)
+    : QObject(parent)
+{
+    // Initialize the compositor here
+}
+
+// Implement any public functions or variables here
+
+// Implement any private functions or variables here
