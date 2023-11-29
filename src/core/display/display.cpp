93fc0cca557f366a9d46bd7b307babb40b607279
Carson Schildt <128015493+cruxschildt@users.noreply.github.com>
2023-11-28 19:09:43 -0700

    Update display.cpp

--- src/core/display/display.cpp
+++ src/core/display/display.cpp
@@ -1,1 +1,12 @@
+// base code
+#include "display.h"
 
+Display::Display(QObject *parent)
+    : QObject(parent)
+{
+    // Initialize the display here
+}
+
+// Implement any public functions or variables here
+
+// Implement any private functions or variables here
