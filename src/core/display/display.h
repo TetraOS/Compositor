65c1df4705aad8898bc84046618a74a2f08a3cda
Carson Schildt <128015493+cruxschildt@users.noreply.github.com>
2023-11-28 19:10:57 -0700

    Update display.h

--- src/core/display/display.h
+++ src/core/display/display.h
@@ -1,1 +1,23 @@
+// display.h
+#pragma once
 
+#include <QObject>
+
+class Display : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit Display(QObject *parent = nullptr);
+
+    // Add any public functions or variables here
+
+signals:
+    // Add any signals here
+
+public slots:
+    // Add any public slots here
+
+private:
+    // Add any private functions or variables here
+};
