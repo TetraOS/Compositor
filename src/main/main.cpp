2418c8ad6ee8e2206b90c9120c41d91e53479d13
carson s <118201@psdschools.org>
2023-12-08 09:45:38 -0700

    Add source code to main.cpp and del. ignore.txt(s)

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,1 +1,34 @@
+#include <QApplication>
+#include <QDebug>
+#include <QThread>
+#include <QWaylandCompositor>
+#include <QWaylandShellSurface>
 
+#include "compositor.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    // Create a QWaylandCompositor
+    QWaylandCompositor compositor;
+
+    // Connect to the shell surface created signal
+    QObject::connect(&compositor, &QWaylandCompositor::shellSurfaceCreated,
+                     [&](QWaylandShellSurface *shellSurface) {
+        // Listen for surface state changes
+        QObject::connect(shellSurface, &QWaylandShellSurface::stateChanged,
+                         [shellSurface] {
+            qDebug() << "Shell surface" << shellSurface << "state changed to"
+                     << shellSurface->state();
+        });
+    });
+
+    // Start the compositor in a new thread
+    QThread compositorThread;
+    compositor.moveToThread(&compositorThread);
+    compositorThread.start();
+
+    // Run the Qt application event loop
+    return app.exec();
+}
