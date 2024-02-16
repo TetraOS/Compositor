15767c5e77a5af74bcca912ded67280b7d83890a
Carson Schildt <128015493+carsonschildt@users.noreply.github.com>
2024-02-15 20:47:02 -0700

    Update compositor.cpp

--- src/core/compositor/compositor.cpp
+++ src/core/compositor/compositor.cpp
@@ -1,13 +1,68 @@
-// This is just the base code, more code will be added later
-
 #include "compositor.h"
+#include <QDebug>
+#include <QWaylandCompositor>
+#include <QWaylandOutput>
+#include <QWaylandSeat>
+#include <QWaylandShellSurface>
+#include <QWaylandSurface>
+#include <QWaylandXdgShell>
+#include <QWaylandXdgShellSurface>
+#include <QWaylandXdgShellV6>
+#include <QWaylandXdgShellV6Surface>
 
-Compositor::Compositor(QObject *parent)
-    : QObject(parent)
-{
-    // Initialize the compositor here
+Compositor::Compositor(QObject *parent) : QObject(parent) {
+    m_compositor = new QWaylandCompositor(this);
+    m_compositor->setOutput(new QWaylandOutput(m_compositor));
+    m_compositor->setSeat(new QWaylandSeat(m_compositor));
+
+    connect(m_compositor, &QWaylandCompositor::surfaceCreated, this, &Compositor::onSurfaceCreated);
+    connect(m_compositor, &QWaylandCompositor::shellCreated, this, &Compositor::onShellCreated);
 }
 
-// Implement any public functions or variables here
+void Compositor::onSurfaceCreated(QWaylandSurface *surface) {
+    qDebug() << "Surface created: " << surface;
+    connect(surface, &QWaylandSurface::destroyed, this, &Compositor::onSurfaceDestroyed);
+    connect(surface, &QWaylandSurface::mapped, this, &Compositor::onSurfaceMapped);
+    connect(surface, &QWaylandSurface::unmapped, this, &Compositor::onSurfaceUnmapped);
+}
 
-// Implement any private functions or variables here
+void Compositor::onShellCreated(QWaylandShell *shell) {
+    qDebug() << "Shell created: " << shell;
+    connect(shell, &QWaylandShell::destroyed, this, &Compositor::onShellDestroyed);
+    connect(shell, &QWaylandShell::shellSurfaceCreated, this, &Compositor::onShellSurfaceCreated);
+}
+
+void Compositor::onSurfaceDestroyed(QWaylandSurface *surface) {
+    qDebug() << "Surface destroyed: " << surface;
+}
+
+void Compositor::onSurfaceMapped(QWaylandSurface *surface) {
+    qDebug() << "Surface mapped: " << surface;
+}
+
+void Compositor::onSurfaceUnmapped(QWaylandSurface *surface) {
+    qDebug() << "Surface unmapped: " << surface;
+}
+
+void Compositor::onShellDestroyed(QWaylandShell *shell) {
+    qDebug() << "Shell destroyed: " << shell;
+}
+
+void Compositor::onShellSurfaceCreated(QWaylandShellSurface *shellSurface) {
+    qDebug() << "Shell surface created: " << shellSurface;
+    connect(shellSurface, &QWaylandShellSurface::destroyed, this, &Compositor::onShellSurfaceDestroyed);
+    connect(shellSurface, &QWaylandShellSurface::mapped, this, &Compositor::onShellSurfaceMapped);
+    connect(shellSurface, &QWaylandShellSurface::unmapped, this, &Compositor::onShellSurfaceUnmapped);
+}
+
+void Compositor::onShellSurfaceDestroyed(QWaylandShellSurface *shellSurface) {
+    qDebug() << "Shell surface destroyed: " << shellSurface;
+}
+
+void Compositor::onShellSurfaceMapped(QWaylandShellSurface *shellSurface) {
+    qDebug() << "Shell surface mapped: " << shellSurface;
+}
+
+void Compositor::onShellSurfaceUnmapped(QWaylandShellSurface *shellSurface) {
+    qDebug() << "Shell surface unmapped: " << shellSurface;
+}
